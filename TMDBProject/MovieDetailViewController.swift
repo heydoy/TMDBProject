386ac665719ca5f8318ee6dd26f558b74015bf75
heydoy <heydoyhey@gmail.com>
2022-08-05 01:39:08 +0900

    무비 상세화면 IB로 구성

---
+++ TMDBProject/MovieDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MovieDetailViewController.swift
+//  TMDBProject
+//
+//  Created by Doy Kim on 2022/08/05.
+//
+
+import UIKit
+
+class MovieDetailViewController: UIViewController {
+    static let identifier = "MovieDetailViewController"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
